dd4cf8119770297ce8a6dada6fcffd136dcda05f
Sufiyani <sufiyanimran55@gmail.com>
2025-09-13 13:43:45 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,6 @@
-
-// import Navbar from "./components/Navbar"
-// import HeroSection from "./components/pages/HeroSection"
-// import Footer from "./components/Footer"
-// // import ServicesLayout2 from "./components/pages/ServicesLayout2"
-// function App() {
-//   return (
-//     <div>
-//       <Navbar />
-//       <HeroSection  />
-// {/* 
-//       <ServicesLayout2 /> */}
-//       <Footer />
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import Topbar from "./components/Topbar";
-
-// Pages
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Pages from "./pages/Pages";
-// import Blog from "./pages/Blog";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<><Topbar /><Home /></>} />  {/* Topbar sirf Home page par */}
-//         <Route path="/about" element={<About />} />
-//         <Route path="/pages" element={<Pages />} />
-//         <Route path="/blog" element={<Blog />} />
-//       </Routes>
-//       <Footer />
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
-// import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import Topbar from "./components/Topbar";
-
-// // Pages
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Pages from "./pages/Pages";
-// import Blog from "./pages/Blog";
-
-// function Layout() {
-//   const location = useLocation();
-
-//   const isHome = location.pathname === "/"; // Check if current page is Home
-
-//   return (
-//     <>
-//       {isHome && <Topbar />}   {/* Sirf Home pr Topbar */}
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/pages" element={<Pages />} />
-//         <Route path="/blog" element={<Blog />} />
-//       </Routes>
-//       {!isHome && <Footer />}  {/* Sirf Home ke alawa pages pr Footer */}
-//     </>
-//   );
-// }
-
-// function App() {
-//   return (
-//     <Router>
-//       <Layout />
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// Common Components
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-// import Topbar from "./components/Topbar";
-// Pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Pages from "./pages/Pages";
@@ -110,15 +13,13 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Home ka apna complete layout */}
         <Route path="/" element={<Home />} />
 
-        {/* Baaki pages ke liye common Navbar + Footer */}
         <Route
           path="/about"
           element={
             <>
-              {/* <Topbar /> */}
+              
               <Navbar />
               <About />
               <Footer />
@@ -129,7 +30,7 @@ function App() {
           path="/pages"
           element={
             <>
-            {/* <Topbar /> */}
+           
               <Navbar />
               <Pages />
               <Footer />
@@ -140,7 +41,18 @@ function App() {
           path="/pages/team-details/:id"
           element={
             <>
-            {/* <Topbar /> */}
+           
+              <Navbar />
+              <PagesTeamDetails />
+              <Footer />
+            </>
+          }
+        />
+         <Route
+          path="/pages/team-details/:id"
+          element={
+            <>
+           
               <Navbar />
               <PagesTeamDetails />
               <Footer />
@@ -151,7 +63,7 @@ function App() {
           path="/blogslayout2"
           element={
             <>
-            {/* <Topbar /> */}
+           
               <Navbar />
               <Blog />
               <Footer />
@@ -163,7 +75,7 @@ function App() {
           path="/serviceslayout2"
           element={
             <>
-            {/* <Topbar /> */}
+           
               <Navbar />
               <ServicesLayout2 />
               <Footer />
@@ -175,7 +87,7 @@ function App() {
           path="/servicesdetails"
           element={
             <>
-            {/* <Topbar /> */}
+           
               <Navbar />
               <ServicesDetails />
               <Footer />
@@ -187,7 +99,7 @@ function App() {
           path="/contact"
           element={
             <>
-            {/* <Topbar /> */}
+           
               <Navbar />
               <Contact />
               <Footer />
